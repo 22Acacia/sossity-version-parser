19a9d19a669968b892bc04064cc62d6dd1b7609c
pat <pat@22acacia.com>
2016-02-04 22:11:35 -0800

    remove file truncation
    this means we can call this file on several config files and the
    output is additive instead of replacing

--- src/version_parser/core.clj
+++ src/version_parser/core.clj
@@ -14,15 +14,13 @@
    ["-o" "--output OUTPUT" "path to output terraform file"]])
 
 (defn -main
-  "Entry Point"
+  "See README for details"
   [& args]
   (let [opts (:options (clojure.tools.cli/parse-opts args cli-options))
         output (:output opts)
         pipelines (:pipelines (conf (:config opts)))
         otherjars (:system-jar-info (:config conf (:config opts)))]
-    ; clear out the file before writing to it
-    (spit output "")
-  
+ 
   
     ; for each pipeline, create <key>/jarname jarname pairs and write to file
     (doseq [[k v] pipelines]
